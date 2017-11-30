f32b8bb56e11931f2c28868d1ced17f52e9d109c
SalcedoJose <josemasal@gmail.com>
2017-11-30 02:41:07 -0800

    Script para RFID modificado para determinar si un identificador es huesped o invitado.

--- rfid/rfid.ino
+++ rfid/rfid.ino
@@ -1,44 +1,66 @@
 /*
- * --------------------------------------------------------------------------------------------------------------------
- * Example sketch/program showing how to read new NUID from a PICC to serial.
- * --------------------------------------------------------------------------------------------------------------------
- * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
- *
- * Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
- * Reader on the Arduino SPI interface.
- *
- * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
- * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
- * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
- * will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
- * when removing the PICC from reading distance too early.
- *
- * @license Released into the public domain.
- *
- * Typical pin layout used:
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
- */
+   --------------------------------------------------------------------------------------------------------------------
+   Example sketch/program showing how to read new NUID from a PICC to serial.
+   --------------------------------------------------------------------------------------------------------------------
+   This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
 
+   Example sketch/program showing how to the read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
+   Reader on the Arduino SPI interface.
+
+   When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
+   then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+   you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
+   will show the type, and the NUID if a new card has been detected. Note: you may see "Timeout in communication" messages
+   when removing the PICC from reading distance too early.
+
+   @license Released into the public domain.
+
+   Typical pin layout used:
+   -----------------------------------------------------------------------------------------
+               MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+               Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+   Signal      Pin          Pin           Pin       Pin        Pin              Pin
+   -----------------------------------------------------------------------------------------
+   RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+   SPI SS      SDA(SS)      10            53        D10        10               10
+   SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+   SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+   SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+*/
+
+// LIBRARIES.
 #include <SPI.h>
 #include <MFRC522.h>
 
+// ARDUINO PINS.
 constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
 constexpr uint8_t SS_PIN = 10;     // Configurable, see typical pin layout above
 
+// RFID class.
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
+// Key used for rfid tags.
 MFRC522::MIFARE_Key key;
 
-// Init array that will store new NUID
+// CONSTANTS.
+// Number of bytes in a UID.
+const byte UID_BYTE_COUNT = 4;
+// Number of existent hosts.
+const byte HOST_COUNT = 2;
+// Number of existent guests.
+const byte GUEST_COUNT = 2;
+
+// Collections to store unique rfid identifiers.
+const byte HOST_UID[HOST_COUNT][4] = {
+  {54, 228, 179, 121}
+  , {144, 231, 136, 73}
+};
+const byte GUEST_UID[GUEST_COUNT][4] = {
+  {38, 248, 82, 165}
+  , {22, 16, 175, 121}
+};
+
+// Init array that will store new NUID.
 byte nuidPICC[4];
 
 void setup() {
@@ -46,6 +68,7 @@ void setup() {
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522
 
+  // Fill key with empty values.
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
@@ -53,15 +76,16 @@ void setup() {
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
   Serial.print(F("Using the following key:"));
   printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+  Serial.println();
 }
 
 void loop() {
 
-  // Look for new cards
+  // Look for new cards.
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
 
-  // Verify if the NUID has been readed
+  // Verify if the NUID has been readed.
   if ( ! rfid.PICC_ReadCardSerial())
     return;
 
@@ -69,46 +93,114 @@ void loop() {
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   Serial.println(rfid.PICC_GetTypeName(piccType));
 
-  // Check is the PICC of Classic MIFARE type
+  // Check if the PICC is of Classic MIFARE type.
   if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
     Serial.println(F("Your tag is not of type MIFARE Classic."));
     return;
   }
 
+  // Check if new card has been placed.
   if (rfid.uid.uidByte[0] != nuidPICC[0] ||
-    rfid.uid.uidByte[1] != nuidPICC[1] ||
-    rfid.uid.uidByte[2] != nuidPICC[2] ||
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+      rfid.uid.uidByte[1] != nuidPICC[1] ||
+      rfid.uid.uidByte[2] != nuidPICC[2] ||
+      rfid.uid.uidByte[3] != nuidPICC[3] ) {
     Serial.println(F("A new card has been detected."));
 
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
+    // Store NUID into nuidPICC array.
+    for (byte i = 0; i < UID_BYTE_COUNT; i++) {
       nuidPICC[i] = rfid.uid.uidByte[i];
     }
 
+    // Display RFID UID.
     Serial.println(F("The NUID tag is:"));
-    Serial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
     Serial.print(F("In dec: "));
     printDec(rfid.uid.uidByte, rfid.uid.size);
     Serial.println();
-  }
-  else Serial.println(F("Card read previously."));
 
-  // Halt PICC
+    // Display the ownership status of the RFID tag.
+    DisplayOwnership(rfid.uid.uidByte);
+    Serial.println();
+    Serial.println();
+  } else {
+    Serial.println(F("Card read previously."));
+    Serial.println();
+  }
+
+  // Halt PICC.
   rfid.PICC_HaltA();
 
-  // Stop encryption on PCD
+  // Stop encryption on PCD.
   rfid.PCD_StopCrypto1();
 }
 
+// FUNCTIONS.
+// Verify if RFID is either a host, guest or neither.
+void DisplayOwnership(byte uid[UID_BYTE_COUNT])
+{
+  // Define ownership to display.
+  String ownership = "does not exist.";
+
+  // Verify if UID is for a host.
+  bool uidIsHost = false;
+  for (byte host = 0; host < HOST_COUNT; host++) {
+    bool currentHostIsUid = true;
+
+    // Loop trough all bytes of the current host ID.
+    for (byte currentByte = 0; currentByte < UID_BYTE_COUNT; currentByte++) {
+      byte currentHostUidByte = HOST_UID[host][currentByte];
+      byte currentUidByte = uid[currentByte];
+      if (currentHostUidByte != currentUidByte) {
+        currentHostIsUid = false;
+        currentByte = UID_BYTE_COUNT;
+      }
+    }
+
+    // If the current host ID is the same as the UID,
+    // define UID as a host and end the loop.
+    if (currentHostIsUid) {
+      uidIsHost = true;
+      host = HOST_COUNT;
+    }
+  }
+
+  // Verify if UID is for a guest.
+  bool uidIsGuest = false;
+  for (byte guest = 0; guest < GUEST_COUNT; guest++) {
+    bool currentGuestIsUid = true;
+
+    // Loop trough all bytes of the current guest ID.
+    for (byte currentByte = 0; currentByte < UID_BYTE_COUNT; currentByte++) {
+      byte currentGuestUidByte = GUEST_UID[guest][currentByte];
+      byte currentUidByte = uid[currentByte];
+      if (currentGuestUidByte != currentUidByte) {
+        currentGuestIsUid = false;
+        currentByte = UID_BYTE_COUNT;
+      }
+    }
+
+    // If the current guest ID is the same as the UID,
+    // define UID as a guest and end the loop.
+    if (currentGuestIsUid) {
+      uidIsGuest = true;
+      guest = GUEST_COUNT;
+    }
+  }
+
+  // Define text if UID is a host or guest.
+  if (uidIsHost)
+    ownership = "is for a host.";
+  else if (uidIsGuest)
+    ownership = "is for a guest.";
+
+  Serial.print("The NUID tag ");
+  Serial.print(ownership);
+}
 
 /**
- * Helper routine to dump a byte array as hex values to Serial.
- */
+   Helper routine to dump a byte array as hex values to Serial.
+*/
 void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
@@ -117,8 +209,8 @@ void printHex(byte *buffer, byte bufferSize) {
 }
 
 /**
- * Helper routine to dump a byte array as dec values to Serial.
- */
+   Helper routine to dump a byte array as dec values to Serial.
+*/
 void printDec(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
